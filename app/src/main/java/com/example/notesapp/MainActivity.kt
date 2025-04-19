71f1f62844ab4eb5efc8a8ca673266dd5cf710d1
JudyChavez <jumachan@hotmail.com>
2025-04-19 12:39:14 -0400

    Repository Test

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             NotesAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android Repository Test",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
